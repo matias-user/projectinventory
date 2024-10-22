ad880f927180e5c639d799b2b67a06f9655728d0
Matías <matias.abarca2018@gmail.com>
2024-10-21 22:38:37 -0300

    Se agrega la vista de crear usuario más el template sin editar css pero funcionando

--- common/urls.py
+++ common/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, create_user
 from django.contrib.auth import views as auth_views
 
 app_name = 'common'
 urlpatterns = [
     path('', home, name='home' ),
-    path ('login/', auth_views.LoginView.as_view(), name='login')
+    path ('login/', auth_views.LoginView.as_view(), name='login'),
+    path('registrate/',  create_user, name='register')
 
 ]
--- common/views.py
+++ common/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .forms import RegisterUserForm
 
 # Create your views here.
 def home(request):
 
     return render(request, 'common/home.html')
+
+
+def create_user(request):
+
+    if request.method == 'POST':
+        form = RegisterUserForm( request.POST or None )
+        form.save()
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'registration/register.html',{'form':form})
