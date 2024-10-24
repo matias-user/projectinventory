8f579bb38281c3201557298070f9306419fa0d3e
Matías <matias.abarca2018@gmail.com>
2024-10-23 21:44:27 -0300

    Se usa la vista de django para cerrar sesión y se modifica el template con un if

--- common/urls.py
+++ common/urls.py
@@ -6,6 +6,7 @@ app_name = 'common'
 urlpatterns = [
     path('', home, name='home' ),
     path ('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout' ),
     path('registrate/',  create_user, name='register')
 
 ]
