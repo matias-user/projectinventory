126363d27704ab416891bb7db9da0d8ad46b14b7
Matías <matias.abarca2018@gmail.com>
2024-10-24 23:41:02 -0300

    Se agregar argumentos a las vistas de login y logout

--- common/urls.py
+++ common/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth import views as auth_views
 app_name = 'common'
 urlpatterns = [
     path('', home, name='home' ),
-    path ('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout' ),
+    path ('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, next_page='common:home'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout' ),
     path('registrate/',  create_user, name='register')
 
 ]
